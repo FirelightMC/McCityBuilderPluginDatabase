f4494f846c0722fa90fb3a77071e1559d2da31a9
Bram Gussekloo <bram.gussekloo@gmail.com>
2021-09-28 16:03:28 +0200

    trying shit

---
+++ src/main/java/com/firelight/McCityBuilderPluginDatabase/Entities/User.java
@@ -0,0 +1,31 @@
+package com.firelight.McCityBuilderPluginDatabase.Entities;
+
+import javax.persistence.*;
+
+@Table(name = "user")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
